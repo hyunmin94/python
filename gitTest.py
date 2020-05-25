2e3322bc4c4af72d7eb32a16b796a6bb886da36e
hyunmin94 <62323327+hyunmin94@users.noreply.github.com>
2020-05-25 20:34:24 +0900

    DB 연동 테스트

--- gitTest.py
+++ gitTest.py
@@ -1,1 +1,10 @@
-print("testFile")
+import pymysql
+
+conn = pymysql.connect(host = '127.0.0.1', port=3306,
+                       user ='root',password='hyunmin')
+
+if conn.open:
+    with conn.cursor() as curs:
+        print("connected")
+
+conn.close()
