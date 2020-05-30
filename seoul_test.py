e318ea5cc7c5b15908b5601f9667624683a8820e
baekji919 <jamie919@naver.com>
2020-05-30 20:47:21 +0900

    서울시 행정구역 좌표(위도, 경도)/(구별)주민등록 인구/사업체수

--- seoul_test.py
+++ seoul_test.py
@@ -33,3 +33,16 @@ columns = ['GIGAN','JACHIGU','SEDAE','GYE_1']
 people_df = pd.DataFrame(data=people_data_list, columns = columns)
 people_df.columns = ['기간','자치구','세대수','인구 합']
 print(people_df)
+
+
+# https://data.seoul.go.kr/dataList/104/A/2/datasetView.do
+# 서울시 사업체현황 (산업대분류별/동별) 통계
+company_url = 'http://openapi.seoul.go.kr:8088/{}/json/octastatapi104/1/500'.format(SEOUL_API_AUTH_KEY)
+company_data_list = seoul_open_api_data(company_url, 'octastatapi104')
+print(company_data_list[1])
+
+columns = ['GIGAN', 'JACHIGU','DONG', 'SAEOPCHESU_1']
+company_df = pd.DataFrame(data=  company_data_list, columns=columns)
+company_df.columns = ['기간', '자치구','동', '사업체 수']
+comp_df = company_df[company_df['동']=='소계']
+print(comp_df)
